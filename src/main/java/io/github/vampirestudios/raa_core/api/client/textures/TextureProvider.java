e0ee275ba2288acbe37e0e8b3cccf03ae07e5ed3
Cat Core <arthur.koalas@free.fr>
2020-05-21 16:58:28 +0200

    Try implementing an API for Textures and Data generation using Artifice

---
+++ src/main/java/io/github/vampirestudios/raa_core/api/client/textures/TextureProvider.java
@@ -0,0 +1,20 @@
+package io.github.vampirestudios.raa_core.api.client.textures;
+
+import com.mojang.serialization.Lifecycle;
+import com.swordglowsblue.artifice.api.ArtificeResourcePack;
+import io.github.vampirestudios.raa_core.RAACore;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.util.registry.SimpleRegistry;
+
+public interface TextureProvider {
+
+    SimpleRegistry<TextureProvider> TEXTURE_PROVIDER_REGISTRY = new SimpleRegistry<>(RegistryKey.getOrCreateRootKey(new Identifier(RAACore.MOD_ID, "texture_provider")), Lifecycle.stable());
+
+    Identifier getId();
+
+    String getAddonId();
+
+    <T> void generateJSONs(T object, ArtificeResourcePack.ClientResourcePackBuilder clientResourcePackBuilder);
+
+}
