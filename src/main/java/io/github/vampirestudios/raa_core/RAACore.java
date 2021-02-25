a2c3b2d6953db1ad2eca06b7398f2fb7bdddd819
OliviaTheVampire <sindrefagerheim53@gmail.com>
2021-02-25 14:56:18 +0100

    added a new config system and wip updated languages

--- src/main/java/io/github/vampirestudios/raa_core/RAACore.java
+++ src/main/java/io/github/vampirestudios/raa_core/RAACore.java
@@ -1,10 +1,6 @@
 package io.github.vampirestudios.raa_core;
 
 import io.github.vampirestudios.raa_core.api.RAAAddon;
-import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
-import me.sargunvohra.mcmods.autoconfig1u.serializer.GsonConfigSerializer;
-import me.sargunvohra.mcmods.autoconfig1u.serializer.JanksonConfigSerializer;
-import me.sargunvohra.mcmods.autoconfig1u.serializer.Toml4jConfigSerializer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import org.apache.logging.log4j.Level;
@@ -28,9 +24,6 @@ public class RAACore implements ModInitializer {
     @Override
     public void onInitialize() {
         log(Level.INFO, String.format("Initializing %s v%s", MOD_NAME, MOD_VERSION));
-        
-        AutoConfig.register(RAACoreConfig.class, JanksonConfigSerializer::new);
-        CONFIG = AutoConfig.getConfigHolder(RAACoreConfig.class).getConfig();
 
         log(Level.INFO, "RAA Addon discovery: Starting");
         FabricLoader.getInstance().getEntrypoints("raa:addon", RAAAddon.class).forEach(raaAddon -> {
--- src/main/java/io/github/vampirestudios/raa_core/RAACoreConfig.java
+++ src/main/java/io/github/vampirestudios/raa_core/RAACoreConfig.java
@@ -1,19 +1,36 @@
 package io.github.vampirestudios.raa_core;
 
+import com.google.gson.JsonObject;
 import io.github.vampirestudios.raa_core.api.name_generation.Language;
-import me.sargunvohra.mcmods.autoconfig1u.ConfigData;
-import me.sargunvohra.mcmods.autoconfig1u.annotation.Config;
-import me.sargunvohra.mcmods.autoconfig1u.shadowed.blue.endless.jankson.Comment;
+import net.fabricmc.fabric.api.config.v1.GsonSerializer;
+import net.fabricmc.loader.api.config.data.SaveType;
+import net.fabricmc.loader.api.config.entrypoint.Config;
+import net.fabricmc.loader.api.config.serialization.ConfigSerializer;
+import net.fabricmc.loader.api.config.value.ValueKey;
 import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.NotNull;
 
-@Config(name=RAACore.MOD_ID)
-public class RAACoreConfig implements ConfigData {
+public class RAACoreConfig extends Config<JsonObject> {
 
-    @Comment("The language names are generated in")
-    public String namingLanguage = Language.LANGUAGE_REGISTRY.getId(Language.ENGLISH).toString();
+    /*This is a test*/
+    public static final ValueKey<String> namingLanguage = value(Language.LANGUAGE_REGISTRY.getId(Language.ENGLISH).toString());
 
     public Language getLanguage() {
-        return Language.LANGUAGE_REGISTRY.get(new Identifier(this.namingLanguage));
+        return Language.LANGUAGE_REGISTRY.get(new Identifier(this.namingLanguage.getValue()));
     }
 
+    @Override
+    public @NotNull ConfigSerializer<JsonObject> getSerializer() {
+        return GsonSerializer.DEFAULT;
+    }
+
+    @Override
+    public @NotNull SaveType getSaveType() {
+        return SaveType.ROOT;
+    }
+
+    @Override
+    public @NotNull String getName() {
+        return "raa-config";
+    }
 }
